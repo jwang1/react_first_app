06e93fb81a8973ee80dfb6554d269dc8d21c3c86
jwang1 <sigkdd@yahoo.com>
2020-06-19 22:58:28 -0400

    use const by default, mutable let second

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let fname = "John";
-  let lname = "Doe";
+  const fname = "John";
+  const lname = "Doe";
   return (
     <div className="App">
       <header className="App-header">
