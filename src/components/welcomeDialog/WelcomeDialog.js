c9cf5d587ca28253c6f17730c4d9fda67134345c
jwang1 <sigkdd@yahoo.com>
2020-07-03 11:51:10 -0400

    updating the message

--- src/components/welcomeDialog/WelcomeDialog.js
+++ src/components/welcomeDialog/WelcomeDialog.js
@@ -5,7 +5,7 @@ export default function WelcomeDialog(props) {
   return (
     <FancyBorder color="blue">
       <h3 className="Dialog-title">Welcome</h3>
-      <h3 className="Dialog-message">This is a simple dialog.</h3>
+      <h3 className="Dialog-message">This is a simple React.js playground.</h3>
     </FancyBorder>
   );
 }
