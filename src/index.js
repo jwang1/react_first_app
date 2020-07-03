3f7a51dfe81bb74df331d082ea62dec68d37c6be
jwang1 <sigkdd@yahoo.com>
2020-07-03 12:23:47 -0400

    adding bootstrap@4.5.0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('hackernews')
+  document.getElementById("hackernews")
 );
 
 // Hot Module Reloading (HMR)
