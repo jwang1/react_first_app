139cd3ceb2aa540cd81c13608c085e70c20a28c4
jwang1 <sigkdd@yahoo.com>
2020-07-05 14:25:54 -0400

    Doing exercise, ES6 reading, the Default Param, and then here, the call function on this and argument list

---
+++ src/es6/defaultParams.js
@@ -0,0 +1,14 @@
+
+function go() {
+  return ":)";
+}
+
+export default function a1(a, b=5, c=b, d=go(), e=this, f=arguments, g=this.value) {
+  return [a, b, c, d, e, f, g];
+}
+
+// result:
+//         [ undefined, 5, 5, ':)', {value: 'happy'}, {}, 'happy' ]
+console.log(a1.call({value: 'happy'}));
+
+console.log("testing ES6 features....");
