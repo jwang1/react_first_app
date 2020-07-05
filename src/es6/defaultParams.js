d56990e85bf8d14bd0829c811a6dca13a0415798
jwang1 <sigkdd@yahoo.com>
2020-07-05 17:06:10 -0400

    update the JavaScript defaultParams.js

--- src/es6/defaultParams.js
+++ src/es6/defaultParams.js
@@ -1,14 +1,24 @@
-
+/* eslint-disable no-undef */
 function go() {
   return ":)";
 }
 
-export default function a1(a, b=5, c=b, d=go(), e=this, f=arguments, g=this.value) {
+function a1(
+  a,
+  b = 5,
+  c = b,
+  d = go(),
+  e = this,
+  f = arguments,
+  g = this.value
+) {
   return [a, b, c, d, e, f, g];
 }
 
 // result:
 //         [ undefined, 5, 5, ':)', {value: 'happy'}, {}, 'happy' ]
-console.log(a1.call({value: 'happy'}));
+console.log(a1.call({ value: "happy" }));
 
 console.log("testing ES6 features....");
+
+console.log(a1.call({ value: "happy" }, []));
